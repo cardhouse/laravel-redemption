4e5413b4c3688a2972c95a95ea6ca55ccb0d73c3
Magic <cardhousemagic@gmail.com>
2021-02-05 00:07:05 -0500

    Added endpoint for redemption counter info

--- routes/api.php
+++ routes/api.php
@@ -2,9 +2,9 @@
 
 use App\Events\ListenerRevoked;
 use App\Events\RedemptionReceived;
+use App\Models\Twitch\Account;
 use App\Models\Twitch\Redemption;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Route;
 use Pusher\Pusher;
 
@@ -28,4 +28,18 @@ Route::post('/twitch/callback', [\App\Http\Controllers\EventSubController::class
 Route::post('/broadcasting', function (Request $request) {
     $pusher = new Pusher(env('PUSHER_APP_KEY'), env('PUSHER_APP_SECRET'), env('PUSHER_APP_ID'));
     return $pusher->socket_auth($request->request->get('channel_name'), $request->request->get('socket_id'));
+});
+
+Route::get('/start/{broadcaster}/counter', function(Request $request, $broadcaster) {
+    $user = Account::find($broadcaster);
+
+    $redemption = app('twitch')
+        ->broadcaster($user)
+        ->getReward($request->input('reward_id'))
+        ->first();
+
+    return response()->json([
+        'name' => $redemption['title'],
+        'count' => $redemption['redemptions_redeemed_current_stream']
+    ]);
 });
